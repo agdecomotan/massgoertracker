7b975da96f79810a237159665895fe27c0ab5419
Ane Decomotan <anelie.decomotan@teloworksinc.com>
2020-06-01 23:00:23 +0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AppComponent} from './app.component';
 
 
 const routes: Routes = [];
+
+
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {path: '', component: HomeComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
